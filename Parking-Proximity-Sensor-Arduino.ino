a8f2b778a4461f37a479bd2ba21c35c770bd9fd1
Christopher Jones <33christopherjones@gmail.com>
2025-08-23 19:10:39 -0700

    Add files via upload

---
+++ Parking-Proximity-Sensor-Arduino.ino
@@ -0,0 +1,58 @@
+//www.elegoo.com
+//2016.12.08
+#include "SR04.h"
+#include "pitches.h"
+
+#define TRIG_PIN 12
+#define ECHO_PIN 10
+#define BLUE 6
+#define GREEN 5
+#define RED 9
+
+SR04 sr04 = SR04(ECHO_PIN,TRIG_PIN);
+long a;
+
+void setup() {
+   Serial.begin(9600);//Initialization of Serial Port
+   delay(1000);
+   pinMode(BLUE, OUTPUT);
+   pinMode(GREEN, OUTPUT);
+   pinMode(RED, OUTPUT);
+   
+}
+
+void loop() {
+
+  if (a < 100) {
+ tone(8, NOTE_D5, 250);}
+
+ for(int i=0; i<3; i++) {
+   a=sr04.Distance();
+   Serial.print(a);
+   Serial.println("cm");//The difference between "Serial.print" and "Serial.println" 
+                        //is that "Serial.println" can change lines.
+ if (a > 50) {
+  analogWrite(GREEN, 100);
+  analogWrite(RED, 0);
+ }
+
+ if (a < 50 && a > 25) {
+  analogWrite(RED, 100);
+  analogWrite(GREEN, 25);
+ }
+
+ if (a < 25) {
+  analogWrite(RED, 100);
+  analogWrite(GREEN, 0);
+ }
+if (a < 100) {
+ delay(a*2.5);}
+ 
+}
+
+ 
+
+
+ 
+
+}
